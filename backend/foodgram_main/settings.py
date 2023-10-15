7e750deef5e25230789c28e4f6b1aac797f97928
Ray-Not <2sinsincuba@gmail.com>
2023-10-15 17:21:13 +0500

    Rm comments

--- backend/foodgram_main/settings.py
+++ backend/foodgram_main/settings.py
@@ -67,12 +67,6 @@ DATABASES = {
         'PORT': os.getenv('POSTGRES_PORT', 5432)
     }
 }
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
 
 AUTH_PASSWORD_VALIDATORS = [
     {
